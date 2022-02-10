5c241a20a45a66d486645bb3e6b0a90eb6c3260e
USER <user@AL01961352.local>
2022-02-10 23:35:23 +0900

    solve 17298 cpp

---
+++ boj/17298/17298.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solve(int n, vector<int> nums)
+{
+	vector<int> answer(n, -1);
+	vector<int> stack;
+	for(int i=0;i<nums.size();i++)
+	{
+		while(!stack.empty() && nums[stack.back()] < nums[i])
+		{
+			answer[stack.back()] = nums[i];
+			stack.pop_back();
+		}
+		stack.push_back(i);
+	}
+
+	return answer;
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> nums(n);
+	for(int i=0;i<n;i++)
+	{
+		cin >> nums[i];
+	}
+
+	vector<int> result = solve(n, nums);
+
+	for(auto val : result)
+	{
+		cout << val << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
